9bd4dbd60f444d350819b38fef77f6e5f365c788
Stephen Wright <stephen.c.wright@gmail.com>
2015-01-25 21:34:57 -0500

    added fancy background

--- js/exhibit.js
+++ js/exhibit.js
@@ -1,1 +1,17 @@
 /** @file exhibit.js */
+
+$(function(){
+
+	// resize all the "walls" on load, and on window resize
+	function wresize(){ $('.wall').css('height', window.innerHeight); }
+	$(window).resize(wresize);
+	wresize();
+
+	$('.wall').each(function(i, el){
+		var src = $(el).find('img')[0].src;
+		$(el)
+			.css('background', 'url('+src+') no-repeat center center fixed')
+			.css('background-size', 'cover');
+	});
+
+});
